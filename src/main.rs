02b2838c964bc1956f873b91cf0e7f90a1dd2691
Niclas Grahm <niclas.grahm@propell.dev>
2026-05-14 21:36:54 +0200

    start wiring up channels

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,76 @@
+use anyhow::Result;
 use axum::{Json, Router, extract::State, http::StatusCode, routing::post};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct NatsInputConfig {
     brokers: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
-enum Input {
+enum InputConfig {
     Nats(NatsInputConfig),
+    Streamer { upstream: StreamerId },
+}
+
+struct BuildCtx<'a> {
+    streamers: &'a mut HashMap<StreamerId, StreamerHandle>,
+}
+
+impl InputConfig {
+    fn build(self, ctx: &mut BuildCtx) -> Result<Input> {
+        match self {
+            InputConfig::Nats(nats_cfg) => {
+                todo!()
+                // Input::Nats(NatsConnection {}),
+            }
+            InputConfig::Streamer { upstream } => {
+                // let (_, rx) = mpsc::channel(100);
+                // Input::Streamer(rx)
+                todo!()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum TransformConfig {}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum OutputConfig {
+    Stdout,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Config {
+    input: InputConfig,
+    transforms: Vec<TransformConfig>,
+    output: OutputConfig,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            input: InputConfig::Nats(NatsInputConfig {
+                brokers: "http://yoyo:4222".to_string(),
+            }),
+            transforms: Vec::new(),
+            output: OutputConfig::Stdout,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NatsConnection {}
+
+#[derive(Debug)]
+enum Input {
+    Nats(NatsConnection),
+    Streamer(tokio::sync::mpsc::Receiver<Arc<serde_json::Value>>),
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -22,36 +81,29 @@ enum Output {
     Stdout,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Config {
-    input: Input,
-    transforms: Vec<Transform>,
-    output: Output,
-}
-
-impl Config {
-    fn new() -> Self {
-        Self {
-            input: Input::Nats(NatsInputConfig {
-                brokers: "http://yoyo:4222".to_string(),
-            }),
-            transforms: Vec::new(),
-            output: Output::Stdout,
-        }
-    }
-}
-
 #[derive(Serialize)]
 struct Streamer {
     id: StreamerId,
     config: Config,
+    #[serde(skip)]
+    input: Input,
+    #[serde(skip)]
+    transforms: Vec<Transform>,
+    #[serde(skip)]
+    output: Output,
+    downstream_senders: Mutex<Vec<mpsc::Sender<Arc<serde_json::Value>>>>,
 }
 
 impl Streamer {
     fn new(config: Config) -> Self {
+        let id = petname::petname(3, "-").unwrap();
         Self {
-            id: "foo".to_string(),
+            id,
             config,
+            input: Input::Nats(NatsConnection {}),
+            transforms: Vec::new(),
+            output: Output::Stdout,
+            downstream_senders: Mutex::new(Vec::new()),
         }
     }
     fn start(&self) -> tokio::task::JoinHandle<()> {
@@ -59,10 +111,16 @@ impl Streamer {
             println!("streamer started");
         })
     }
+    fn subscribe(&mut self, tx: mpsc::Sender<Arc<serde_json::Value>>) -> Result<()> {
+        let mut senders = self.downstream_senders.get_mut()?;
+        senders.push(tx);
+        Ok(())
+    }
 }
 type StreamerId = String;
+type StreamerHandle = tokio::task::JoinHandle<()>;
 struct AppState {
-    streamers: Mutex<HashMap<StreamerId, tokio::task::JoinHandle<()>>>,
+    streamers: Mutex<HashMap<StreamerId, StreamerHandle>>,
 }
 
 #[tokio::main]
@@ -74,14 +132,14 @@ async fn main() {
         .route("/", post(create_stream))
         .with_state(Arc::new(state));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6767").await.unwrap();
-    axum::serve(listener, app).await;
+    let _ = axum::serve(listener, app).await;
 }
 
 async fn create_stream(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<Config>,
 ) -> (StatusCode, Json<Streamer>) {
-    let streamer = Streamer::new(payload);
+    let streamer = Streamer::new(payload.clone());
     let streamer_handle = streamer.start();
     let mut app = state.streamers.lock().unwrap();
     app.insert(streamer.id.clone(), streamer_handle);
