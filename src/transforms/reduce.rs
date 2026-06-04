ac0a6e8aa9830c4dd16e9a20cfcae0c83e112685
Niclas Grahm <niclas.grahm@propell.dev>
2026-06-04 15:43:54 +0200

    added a reducer transform

--- src/transforms/reduce.rs
+++ src/transforms/reduce.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use crate::{
     BuildCtx,
@@ -20,6 +21,7 @@ enum ReduceFnKind {
 pub struct ReduceTransformConfig {
     function: ReduceFnKind,
     field: String,
+    new_field_name: String,
 }
 impl BuildTransform for ReduceTransformConfig {
     fn build(self, ctx: &mut BuildCtx) -> anyhow::Result<Box<dyn Transform>> {
@@ -41,6 +43,35 @@ impl Transform for ReduceTransform {
         &mut self,
         message_batch: Arc<MessageBatch>,
     ) -> anyhow::Result<Vec<Arc<MessageBatch>>> {
-        todo!()
+        let values: Vec<f64> = message_batch
+            .iter()
+            .map(|msg| msg.get(&self.field).unwrap().as_f64().unwrap())
+            .collect();
+
+        let reduced = match self.function {
+            ReduceFnKind::Sum => values.iter().sum::<f64>(),
+            ReduceFnKind::Avg => {
+                let top = values.iter().sum::<f64>();
+                let bottom = values.len() as f64;
+                // handle division by zero
+                top / bottom
+            }
+            ReduceFnKind::Min => values.iter().copied().reduce(f64::min).unwrap(),
+            ReduceFnKind::Max => values.iter().copied().reduce(f64::max).unwrap(),
+        };
+
+        let msg = serde_json::to_value(&ReduceOutputFormat {
+            original_field: self.field.clone(),
+            reduced_value: reduced,
+        })?;
+
+        let batch: MessageBatch = vec![Arc::new(msg)];
+        Ok(vec![Arc::new(batch)])
     }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct ReduceOutputFormat {
+    original_field: String,
+    reduced_value: f64,
 }
